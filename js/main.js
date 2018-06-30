b8ca7aa2868d8658bbef7fdaf5f173009e9ab53b
horstmannd <dustin.horstmann@gmail.com>
2018-06-30 14:15:00 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//@prepros-prepend jquery.dev.js
+//@prepros-prepend tweenmax.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend ie10-viewport.js
+
+$(document).ready(function(){
+
+  // Back., Elastic., Bounce., SlowMo.
+  
+  TweenMax.from('.jumbotron', 1, {opacity:0});
+	TweenMax.from('.navbar-brand', .5, {top: -50, opacity: 0, ease:Power0.easeOut}); 
+	TweenMax.from('.jumbotron img', 1, {bottom: 0, opacity: 0, ease:Power0.easeOut})
+});
